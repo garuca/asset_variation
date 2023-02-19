e6ee5ae5cc35923b59bdb3e230669750c19955fe
garua <gabriel.rufino.carvalho@gmail.com>
2023-02-19 03:35:01 -0300

    refactor

---
+++ lib/feature/stock/history/data/repositories/stock_history_repository_impl.dart
@@ -0,0 +1,21 @@
+import 'package:asset_variation/feature/stock/history/data/data_source/stock_history_data_source.dart';
+import 'package:asset_variation/feature/stock/history/domain/entities/stock_history.dart';
+import 'package:asset_variation/feature/stock/history/domain/errors/errors.dart';
+import 'package:asset_variation/feature/stock/history/domain/repositories/stock_history_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class StockHistoryRepositoryImpl implements StockHistoryRepository {
+  final StockHistoryDataSource dataSource;
+
+  StockHistoryRepositoryImpl(this.dataSource);
+
+  @override
+  Future<Either<Failure, StockHistory?>> get() async {
+    try {
+      final result = await dataSource.get();
+      return result;
+    } catch (e) {
+      return Left(DataSourceError());
+    }
+  }
+}
--- lib/feature/stock/history/domain/repositories/stock_history_repository.dart
+++ lib/feature/stock/history/domain/repositories/stock_history_repository.dart
@@ -3,5 +3,5 @@ import 'package:asset_variation/feature/stock/history/domain/errors/errors.dart'
 import 'package:dartz/dartz.dart';
 
 abstract class StockHistoryRepository {
-  Future<Either<Failure, StockHistory>> get();
+  Future<Either<Failure, StockHistory?>> get();
 }
